7df71a27859a14bdb62d9449b8cbf7b62078d5c6
Matt Bosworth <matt@codetastic.com>
2021-05-30 01:39:59 -0700

    Added support for google photos access tokens from refresh tokens

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require("firebase-functions");
-const got = require('got')
+const got = require('got');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,8 +14,73 @@ const request = require('request');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// MSB: attempt to refresh the spotify api token
+exports.getToken = functions.https.onCall(async (req, res) => {
 
-// Take a text parameter passed ot the HTTP endpoint 
+  // requesting access token from refresh token
+  return admin.database()
+        .ref("/spotify")
+        .once('value').then( snapshot => {
+    let refresh_token = snapshot.val().refresh_token;
+    let client_id = snapshot.val().client_id;
+    let client_secret = snapshot.val().client_secret;
+
+    const url = 'https://accounts.spotify.com/api/token';
+    const authOptions = {
+      method: 'post',
+      headers: { 'Authorization': 'Basic ' +
+        (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      form: {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+      }
+    };
+
+    return got.post(url, authOptions).json().then( json => {
+        functions.logger.log('Got result', json.access_token);
+        admin.database().ref("spotify/accessToken").set(json.access_token);
+        return {access_token: json.access_token};
+    });
+  });
+});
+
+// MSB: attempt to refresh the google photos api token
+exports.getPhotosToken = functions.https.onCall(async (req, res) => {
+
+  // requesting access token from refresh token
+  return admin.database()
+        .ref("/gphotos")
+        .once('value').then( snapshot => {
+    let refresh_token = snapshot.val().refresh_token;
+    let client_id = snapshot.val().client_id;
+    let client_secret = snapshot.val().client_secret;
+
+    const url = 'https://oauth2.googleapis.com/token';
+    const authOptions = {
+      method: 'post',
+      headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+      },
+      form: {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+      }
+    };
+
+    return got.post(url, authOptions).json().then( json => {
+        functions.logger.log('Got result', json.access_token);
+        admin.database().ref("gphotos/accessToken").set(json.access_token);
+        return {access_token: json.access_token};
+    });
+  });
+});
+
+
+/*
+// Take a text parameter passed to the HTTP endpoint 
 // and insert it into Firestore under the path /messages/:documentId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
     //Grab the text parameter
@@ -47,73 +112,5 @@ exports.makeUppercase = functions.firestore.document(`/messages/{documentId}`)
         // Promise.
         return snap.ref.set({uppercase}, {merge: true});
     });
+*/
 
-// MSB: attempt to refresh the spotify api token
-// app.get('/refresh_token', function(req, res) {
-//exports.getToken = functions.https.onRequest(async (req, res) => {
-//exports.getToken = functions.https.onRequest(async (req, res) => {
-exports.getToken = functions.https.onCall(async (req, res) => {
-
-  // requesting access token from refresh token
-  return admin.database()
-        .ref("/spotify")
-        .once('value').then( snapshot => {
-    let refresh_token = snapshot.val().refresh_token;
-    let client_id = snapshot.val().client_id;
-    let client_secret = snapshot.val().client_secret;
-
-    const url = 'https://accounts.spotify.com/api/token';
-    const authOptions = {
-      method: 'post',
-      headers: { 'Authorization': 'Basic ' +
-        (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      form: {
-        'grant_type': 'refresh_token',
-        'refresh_token': refresh_token
-      }
-    };
-
-    return got.post(url, authOptions).json().then( json => {
-        functions.logger.log('Got result', json.access_token);
-        admin.database().ref("spotify/accessToken").set(json.access_token);
-        return {access_token: json.access_token};
-    });
-  });
-});
-
-// Old way
-exports.getToken2 = functions.https.onRequest(async (req, res) => {
-
-  // requesting access token from refresh token
-  const access_token = admin.database()
-        .ref("/spotify")
-        .once('value', function(snapshot) {
-    let refresh_token = snapshot.val().refresh_token;
-    let client_id = snapshot.val().client_id;
-    let client_secret = snapshot.val().client_secret;
-
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: { 'Authorization': 'Basic ' +
-        (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      form: {
-        grant_type: 'refresh_token',
-        refresh_token: refresh_token
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        access_token = body.access_token;
-        functions.logger.log('Got access token', access_token);
-        res.json(access_token)
-      } else {
-        functions.logger.log("Problem getting token", error);
-        res.json({access_token: "problem"});
-      }
-    });
-  });
-});
